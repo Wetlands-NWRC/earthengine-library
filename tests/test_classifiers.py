ef6d12b22d7a596eeadff9315a7a0e5ee70dfe86
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-11-17 11:19:18 -0500

    added new RF test classes

---
+++ tests/test_classifiers.py
@@ -0,0 +1,15 @@
+import unittest
+
+import ee
+
+
+class TestRandomForest(unittest.TestCase):
+
+    def test_rf_classifer_construction(self):
+        pass
+
+    def test_rf_classifier_train_classifier(self):
+        pass
+
+    def test_apply_rf_model_to_image(self):
+        pass
