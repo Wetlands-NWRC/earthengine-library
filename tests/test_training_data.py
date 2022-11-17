8f989090a553bd95be133e341525469b47e9d810
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-11-17 11:41:26 -0500

    stubbed out test cases

---
+++ tests/test_training_data.py
@@ -0,0 +1,17 @@
+import unittest
+
+import ee
+
+from eelib import td
+
+
+class TestTrainingPointSamples(unittest.TestCase):
+
+    def test_construciton_of_training_sample(self):
+        pass
+
+    def test_return_type_ee_FeatureCollection_true(self):
+        pass
+
+    def test_construction_geometry_multipoint_true(self):
+        pass
