232be85c5e5777dc5cd81bb540e52566cc324593
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-11-17 15:12:05 -0500

    all test passing

--- tests/test_training_data.py
+++ tests/test_training_data.py
@@ -7,11 +7,42 @@ from eelib import td
 
 class TestTrainingPointSamples(unittest.TestCase):
 
-    def test_construciton_of_training_sample(self):
-        pass
+    def test_construciton_of_training_sample_multi_point(self):
+        training_data = ee.FeatureCollection(
+            "users/ryangilberthamilton/BC/widgeon/3Class/wd_tr_3")
+        image = ee.Image(
+            "users/ryangilberthamilton/BC/widgeon/stacks/widgeon_stack_2019")
+
+        try:
+            td.TrainingPointSamples(
+                image=image,
+                collection=training_data,
+                scale=10,
+                tile_scale=16
+            )
+        except Exception as e:
+            print(e)
+            self.fail("Cannot construct Obj")
 
     def test_return_type_ee_FeatureCollection_true(self):
-        pass
+        training_data = ee.FeatureCollection(
+            "users/ryangilberthamilton/BC/widgeon/3Class/wd_tr_3")
+        image = ee.Image(
+            "users/ryangilberthamilton/BC/widgeon/stacks/widgeon_stack_2019")
 
-    def test_construction_geometry_multipoint_true(self):
-        pass
+        training_ds = td.TrainingPointSamples(
+            image=image,
+            collection=training_data,
+            scale=10,
+            tile_scale=16
+        )
+
+        training_ds.sample()
+
+        smpl = training_ds.get_trianing_samples()
+
+        try:
+            smpl.first().getInfo()
+        except Exception as e:
+            print(e)
+            self.fail("Cannot construct Obj")
