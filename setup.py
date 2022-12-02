158dc2fe1b8823a5a4476882a33e61e5ad1cc282
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-02 13:09:54 -0500

    remove setup.cfg

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 if __name__ == "__main__":
-    setup()
+
+    setup(
+        name='earth-engine-library',
+        author="Ryan Hamilton",
+        packages=find_packages(exclude=('tests*'))
+    )
