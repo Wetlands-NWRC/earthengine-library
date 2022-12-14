99ac1fd5c2421e4b488f4ffba5c80e0d3d34fb86
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-14 09:57:08 -0500

    add doc string

--- eelib/bands.py
+++ eelib/bands.py
@@ -31,15 +31,22 @@ class S2SR(Enum):
     B12 = 'SWIR 2'
 
 
-class S2DC(Enum):
-    B0 = 'Aerosols'
-    B1 = 'Blue'
-    B2 = 'Green'
-    B3 = 'Red'
-    B4 = 'Red Edge 1'
-    B5 = 'Red Edge 2'
-    B6 = 'Red Edge 3'
-    B7 = 'NIR'
-    B8 = 'Red Edge 4'
-    B9 = 'SWIR 1'
-    B10 = 'SWIR 2'
+class S2DCParsed(Enum):
+    """After a band offset has been added and Composite has been parsed into
+    seperate Seasonal Composites"""
+    B1 = 'Aerosols'
+    B2 = 'Blue'
+    B3 = 'Green'
+    B4 = 'Red'
+    B5 = 'Red Edge 1'
+    B6 = 'Red Edge 2'
+    B7 = 'Red Edge 3'
+    B8 = 'NIR'
+    B9 = 'Red Edge 4'
+    B10 = 'SWIR 1'
+    B11 = 'SWIR 2'
+
+
+class S2DataCube(Enum):
+    """Original Data Cube Band Mappings"""
+    pass
