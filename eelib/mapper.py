784c2122bd7263977a51c38dfdf5cfc6b371f333
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-19 11:08:40 -0500

    some changes

--- eelib/mapper.py
+++ eelib/mapper.py
@@ -18,7 +18,17 @@ class eeMapper(folium.Map):
                          zoom_control, **kwargs)
 
     def add_ee_layer(self, ee_image_object, vis_params, name):
-        map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+        if isinstance(ee_image_object, ee.ImageCollection):
+            map_id_dict = ee.Image(
+                ee_image_object.mosaic()).getMapId(vis_params)
+
+        elif isinstance(ee_image_object, ee.FeatureCollection):
+            map_id_dict = ee.FeatureCollection(
+                ee_image_object).getMapId(vis_params)
+
+        else:
+            map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+
         folium.raster_layers.TileLayer(
             tiles=map_id_dict['tile_fetcher'].url_format,
             attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
