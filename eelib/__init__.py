b03ffadd9bad1c17de5f7eadcdf99afdb8308c22
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-21 11:35:46 -0500

    add bounded methods

--- eelib/__init__.py
+++ eelib/__init__.py
@@ -1,4 +1,16 @@
 import ee
 
+from . import eefactory
+
 if not ee.data._credentials:
     ee.Initialize()
+
+# add class factory methods to ee.ImageCollection and ee.FeatureCollection
+fc_base = ee.FeatureCollection
+
+fc_base.from_dataframe = eefactory.from_dataframe
+fc_base.from_file = eefactory.from_file
+fc_base.from_image_collection = eefactory.from_image_collection
+fc_base.to_file = eefactory.to_file
+
+# Image Collection Factory class methods
