5de7d80a0439ea5043233bfd9c6c88934e2b4e3e
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-21 22:14:42 -0500

    bounded class methods to ee.ImageCollection

--- eelib/__init__.py
+++ eelib/__init__.py
@@ -14,3 +14,7 @@ fc_base.from_image_collection = eefactory.from_image_collection
 fc_base.to_file = eefactory.to_file
 
 # Image Collection Factory class methods
+ic_base = ee.ImageCollection
+ic_base.s1Collection = eefactory.s1Collection
+ic_base.s2TOACollection = eefactory.s2TOACollection
+ic_base.s2SRCollection = eefactory.s2SRCollection
