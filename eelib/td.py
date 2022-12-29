27d57a70900195781a9e237394f7fbece75022b3
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-29 10:02:17 -0500

    add collection prop to training data class

--- eelib/td.py
+++ eelib/td.py
@@ -10,8 +10,10 @@ class TrainingData:
     samples: ee.FeatureCollection
     class_labels: str = field(init=True, repr=True, default='land_cover')
     class_values: str = field(init=True, repr=True,
-                              default='land_cover_values')
+                              default='land_value')
     _samples: ee.FeatureCollection = field(
+        init=False, repr=False, default=None)
+    _collection: ee.FeatureCollection = field(
         init=False, repr=False, default=None)
 
     @property
@@ -24,6 +26,17 @@ class TrainingData:
         if type(value) is property:
             value = TrainingData._samples
         self._samples = value
+
+    @property
+    def collection(self):
+        """The collection property."""
+        return self._collection
+
+    @collection.setter
+    def collection(self, value):
+        if type(value) is property:
+            value = TrainingData._collection
+        self._collection = value
 
 
 def training_samples(image: ee.Image, training_data: TrainingData,
