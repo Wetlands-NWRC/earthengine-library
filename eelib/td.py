b90c8fc1b8caccf7adb8064905e20a88a336ddc0
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-28 22:18:53 -0500

    add training data class

--- eelib/td.py
+++ eelib/td.py
@@ -1,8 +1,23 @@
 from abc import ABC
-from pprint import pprint
 from typing import List, Union
 
 import ee
+
+
+# @TODO convert to dataclass
+class TrainingData:
+    def __init__(self, collection: ee.FeatureCollection) -> None:
+        self.collection = collection
+        self._label_col = 'land_cover'
+
+    @property
+    def class_labels(self):
+        """The class_labels property."""
+        return self._class_labels
+
+    @class_labels.setter
+    def class_labels(self, value):
+        self._class_labels = value
 
 
 class TrainingSample(ABC):
@@ -11,16 +26,16 @@ class TrainingSample(ABC):
 
 class TrainingSamples(TrainingSample):
 
-    def __init__(self, image: ee.Image, collection: ee.FeatureCollection,
+    def __init__(self, image: ee.Image, training_data: TrainingData,
                  properties: List[str] = None, scale: float = None,
                  projection: ee.Projection = None, tile_scale: float = 1,
                  geometries: bool = False):
-        test_geometry_type = collection.geometry().type().getInfo()
+        test_geometry_type = training_data.collection.geometry().type().getInfo()
         if test_geometry_type != 'MultiPoint':
             raise ee.EEException("Collection is not of type MultiPoint")
 
         self._image = image
-        self._collection = collection
+        self._collection = training_data.collection
         self._properties = properties
         self._scale = scale
         self._projection = projection
