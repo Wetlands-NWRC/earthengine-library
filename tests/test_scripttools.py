7698d9210c1ee477f040d7cdadd8963d966e865f
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-10 14:11:54 -0500

    test passing

--- tests/test_scripttools.py
+++ tests/test_scripttools.py
@@ -1,9 +1,12 @@
+import os
 import unittest
 
 import ee
+import pandas as pd
 
-from eelib.scripttools import moa
+from eelib.scripttools import eemoa, extract_by_rank, plot_moa_dis
 
+CURRENT_DIS = os.path.abspath(os.path.dirname(__file__))
 
 class TestMoa(unittest.TestCase):
 
@@ -14,10 +17,46 @@ class TestMoa(unittest.TestCase):
         pts = ee.FeatureCollection(
             'users/ryangilberthamilton/BC/williston/fpca/willistonA_no_floodplain')
 
-        moa_table = moa(
+        moa_table = eemoa(
             image=image,
             label_col=label_column,
             pts=pts
         )
 
         self.fail(moa_table.first().getInfo())
+
+    def test_moa_extract_bands(self):
+        os.chdir(CURRENT_DIS)
+        
+        moa_csv = "../testing_data/moa_script_tool_test.csv"
+        scores_csv = "../testing_data/Samples-Test.csv"
+        
+        df_moa = pd.read_csv(moa_csv)
+        df_scores = pd.read_csv(scores_csv)
+        
+        RANK = 1
+
+        try:
+            predictors = extract_by_rank(df_moa, RANK)
+        except Exception:
+            self.fail("failed")
+        
+    def test_moa_plot(self):
+        os.chdir(CURRENT_DIS)
+        
+        moa_csv = "../testing_data/moa_script_tool_test.csv"
+        scores_csv = "../testing_data/Samples-Test.csv"
+        
+        df_moa = pd.read_csv(moa_csv)
+        df_scores = pd.read_csv(scores_csv)
+        
+        RANK = 1
+        predictors = extract_by_rank(df_moa, RANK)
+        try:
+            plot_moa_dis(
+                predictors=predictors,
+                sample_table=df_scores,
+                dir="../testing_data/plotting"
+            )
+        except Exception:
+            self.fail("Failed")
