64a56aa1f2614f6aa5e22a2974f9fec80ada05ab
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-19 09:41:44 -0500

    rework moa calculator to a Moa table obj added methods

--- eelib/scripttools.py
+++ eelib/scripttools.py
@@ -67,49 +67,90 @@ def eemoa(image: ee.Image, label_col: str, pts: ee.FeatureCollection) -> ee.Feat
     return ee.FeatureCollection(collections).flatten()
 
 
-def extract_by_rank(moa_table: pd.DataFrame, rank: int = 1) -> Dict[str, pd.Series]:
-    df = moa_table[moa_table['01_Rank'] == 1]
-    bands = df['02_Band'].unique().tolist()
-    return bands
 
-def plot_moa_dis(predictors: List[Any], sample_table: pd.DataFrame, 
-                 dir: str = None) -> None:
-    
-    dir = './plotting' if dir is None else dir
-    
-    def hist_factory(series: pd.Series, title: str, bin: Union[str, int] = None):
-        bin = 'auto' if bin is None else bin
-        
-        n, bins, patches = plt.hist(series, bin, rwidth=0.85)
-
-        plt.xlabel('Value')
-        plt.ylabel('Frequency')
-        plt.title(title)
-
-        # plt.xlim(-1, 1)
-        # plt.ylim(0, 700)
-        maxfreq = n.max()
-        plt.ylim(ymax=np.ceil(maxfreq / 100) * 100 if maxfreq % 100 
-                 else maxfreq + 100)
-        
-        plt.grid(True)
-        return n, bins, patches
-
-    for band in predictors:
-        series = sample_table[band]
-        
-        hist_factory(
-            series=series,
-            title=band,
-            bin=100
-        )
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-        plotname = os.path.join(dir, f'{band}.png')
-        
-        plt.savefig(plotname)
-        plt.close()
-    return None
     # Step 3: select the collection based on do lazy loading
 
+
+class MoaTable(pd.DataFrame):
+    def __init__(self, dfin: pd.DataFrame, label: str) -> None:
+        labels = dfin[label].unique().tolist()
+        # set combinations
+        label_combs = combinations(labels, 2)
+        
+        # group dataframe by land cover
+        land_covers = {land_cover: dfin[(dfin[label] == land_cover)] for land_cover in labels}
+        
+        column = [i for i in dfin.columns if i not in ['.geo', 'system:index', 'CID', label]]
+        
+        dfs = []
+        for comb in label_combs:
+            ref = land_covers.get(comb[0])
+            trg = land_covers.get(comb[1])
+            
+            scores = []
+            for land_cover in column:
+                a1 = ref[land_cover].to_numpy()
+                a2 = trg[land_cover].to_numpy()
+                
+                a1m = a1.mean()
+                a2m = a2.mean()
+                
+                cat = np.concatenate((a1, a2))
+                
+                total_std = cat.std()
+                
+                value = abs((a2m - a1m) / total_std)
+                scores.append(value)
+
+            array = np.array(scores)
+            dfmoa = pd.DataFrame(data={'band': column, 'scores': array})
+            df_sort = dfmoa.sort_values(by='scores', ascending=False)
+            df_sort['rank'] = np.arange(1, len(column) + 1)
+            df_sort['labels'] = [f'{comb[0]}:{comb[1]}' for _ in range(0, len(column))]
+            dfs.append(df_sort)
+        moa_table = pd.concat(dfs, axis=0, ignore_index=True)
+        moa_table = moa_table[['labels', 'rank', 'band', 'scores']]
+        super().__init__(moa_table)
+        self._samples = dfin
+
+    def plot_moa_dis(self, predictors: List[Any], dir: str = None) -> None:
+        
+        dir = './plotting' if dir is None else dir
+        
+        def hist_factory(series: pd.Series, title: str, bin: Union[str, int] = None):
+            bin = 'auto' if bin is None else bin
+            
+            n, bins, patches = plt.hist(series, bin, rwidth=0.85)
+
+            plt.xlabel('Value')
+            plt.ylabel('Frequency')
+            plt.title(title)
+
+            maxfreq = n.max()
+            plt.ylim(ymax=np.ceil(maxfreq / 100) * 100 if maxfreq % 100 
+                    else maxfreq + 100)
+            
+            plt.grid(True)
+            return n, bins, patches
+
+        for band in predictors:
+            series = self._samples[band]
+            
+            hist_factory(
+                series=series,
+                title=band,
+                bin=100
+            )
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            plotname = os.path.join(dir, f'{band}.png')
+            
+            plt.savefig(plotname)
+            plt.close()
+        return None
+
+    def extract_by_rank(self, rank: int = 1) -> List[Any]:
+        df = self[self['rank'] == rank]
+        bands = df['band'].unique().tolist()
+        return bands
